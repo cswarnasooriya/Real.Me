f39c192c6f04508c966580006101c46a094681d8
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-05-12 13:56:29 +0530

    Home section updated

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { Carousel } from "flowbite-react";
-// import banner from '../assets/banner.png'
+import banner from '../assets/banner_1.png'
 import '../App.css';
 
 const Home = () => {
@@ -7,16 +7,54 @@ const Home = () => {
     <div className="bg-neutralSilver ">
         <div className="px-4 lg:px-14 max-w-screen mx-auto min-h-screen h-screen">
 
-        <Carousel>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
+        <Carousel className="w-full mx-auto">
+
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+          <div>
+            <img src={banner} alt=""  />
+          </div>
+
+          {/* hero text */}
+
+          <div className=" md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4  leading-snug">Lessons and Insights <span className="text-brandPrimary leading-snug"> from 8 years.</span></h1>
+
+            <p className="text-neutralGrey mb-8 text-base">
+              Where to grow your business as a Photographer: site or social media?
+            </p>
+
+            <button className="btn-primary">
+              Register
+            </button>
+          </div>
+
+          
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
+
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+          <div>
+            <img src={banner} alt=""  />
+          </div>
+
+          {/* hero text */}
+
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4  leading-snug">Lessons and Insights <span className="text-brandPrimary leading-snug"> from 8 years.</span></h1>
+
+            <p className="text-neutralGrey mb-8 text-base">
+              Where to grow your business as a Photographer: site or social media?
+            </p>
+
+            <button className="btn-primary">
+              Register
+            </button>
+          </div>
+
+          
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
-        </div>
+
+        
+        
       </Carousel>
       </div>
       
